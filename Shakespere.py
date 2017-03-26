8664f2e6793ded019a7c73560ee2d0bf539f6ead
Connor Perill <connorjperill@gmail.com>
2017-03-27 00:05:39 +0100

    First vanilla attempt - very slow
    This is a basic generator, no saving of results. Very slow and useless.

--- Shakespere.py
+++ Shakespere.py
@@ -3,47 +3,70 @@ Author:     Connor Perill
 Program:    String Generator
 Usage:      Generates and compares "methinks it is like a weasel"
                 Functions:
-                    StringGenerator
-                    StringScore
-                    StringCompare
+                    generator
+                    genscore (will score each letter out of 27 - next phase)
+                    compare
+                    
+Lesson: http://interactivepython.org/runestone/static/pythonds/Introduction/DefiningFunctions.html
 """
 
 import string
 import random
 
-goal="methinks it is like a weasel"
-choices=["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"," "]
+goal = "methinks it is like a weasel"
+choices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u",
+           "v", "w", "x", "y", "z", " "]
 
-def stringGenerator():
-    genString=''
-    go=True
+
+def generator():
+    gen = ''
+    go = True
     count = 0
-    while go==True:
+    while go:
         while count<28:
-            '''num=random.randint(0,1)
-            if num == 1:
-                genString += random.choice(string.letters)
-            else:
-                genString += " "
-            '''
-            x=random.choice(choices)
-            print(x)
-            genString += x
+            x = random.choice(choices)
+            gen += x
             count += 1
-        go=False
-    return genString
+        go = False
+    return gen
 
-def stringScore(s):
-    score=0
+
+def genscore(s):
+    score = 0
+    if goal == s:
+        score = 100
+
     return score
 
-def stringCompare(goal, string):
-    return
+
+def compare(genstr):
+
+    if goal == genstr:
+        return True
+    else:
+        return False
+
 
 def start():
-    print("Program started")
-    "print(string.lowercase)"
+    working = True
 
-    test = stringGenerator()
+    print("Program started")
+    print("Generating string...")
+
+    count = 0
+    while working:
+
+        gen = generator()
+        result = compare(gen)
+
+        if result:
+            working = False
+            print("***STRING COMPLETE***")
+            print("Results: %s (goal) equals %s (generated string)" % (goal, gen))
+        else:
+            count = count + 1
+            if count % 100000 == 0:
+                print("Count: %s" % count)
+
 
 start()
